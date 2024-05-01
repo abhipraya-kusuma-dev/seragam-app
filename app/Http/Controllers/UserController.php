3d74fa4835b15a6ccf09f2ff84c548bcbcb86365
AdiCahyaSaputra <adi.jsonparse@gmail.com>
2024-05-01 12:18:58 +0700

    docs: update readme

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,23 +7,23 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    public function login(Request $request){
-        $incomingFields = $request -> validate([
-            'name' => ['required', 'exists:users,name'],
-            'password' => ['required']
-        ]);
-    
-        if(Auth::attempt($incomingFields)){
-            return redirect()->intended('/beranda');
-        } else {
-            return back()->withErrors(['password' => 'invalidCredentials']);
-        }
+  public function login(Request $request)
+  {
+    $incomingFields = $request->validate([
+      'name' => ['required', 'exists:users,name'],
+      'password' => ['required']
+    ]);
 
-        
+    if (Auth::attempt($incomingFields)) {
+      return redirect()->intended('/beranda');
+    } else {
+      return back()->withErrors(['password' => 'invalidCredentials']);
     }
+  }
 
-    public function logout(){
-        Auth::logout();
-        return redirect()->intended('/login');
-    }
+  public function logout()
+  {
+    Auth::logout();
+    return redirect()->intended('/login');
+  }
 }
