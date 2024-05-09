34ae5841c92c737782ddfd4187bd878561955877
Fakhry <anteikuwute@gmail.com>
2024-05-09 19:51:10 +0700

    feat: table lihat order DOM

--- resources/views/gudang/order-detail.blade.php
+++ resources/views/gudang/order-detail.blade.php
@@ -18,7 +18,7 @@
             @if (session('update-error'))
                 <p class="text-red-600">{{ session('update-error') }}</p>
             @endif
-            <h3 class="font-bold mb-4">Jenjang</h3>
+            <h3 class="font-bold mb-1">Jenjang</h3>
             <div class="pin-jenjang  mb-4">
                 @if ($order->jenjang === 'sd')
                     <h1 class="font-semibold text-white bg-[#FF5656] text-center rounded-[8px] py-1 w-[81px]"
@@ -44,29 +44,85 @@
 
         </div>
         <div>
-            <form action="/gudang/order/{{ $nomor_urut }}/update" method="POST">
+            <form action="/gudang/order/{{ $nomor_urut }}/update" method="POST" class="flex items-start gap-10">
                 @method('PUT')
                 @csrf
 
-                <div class="grid grid-cols-1 gap-2">
-                    @foreach ($order->seragams as $seragam)
-                        <div class="p-2 border">
-                            <label for="{{ $seragam->id }}">{{ $seragam->nama_barang }}</label>
-                            <input type="checkbox" id="{{ $seragam->id }}" value="{{ $seragam->id }}" name="seragam_ids[]"
-                                {{ $seragam->tersedia ? 'checked' : '' }} />
-                        </div>
-                    @endforeach
+                <div class="grid grid-cols-1 gap-2 ">
+                    
+                       <table class="w-[541px]">
+                            <thead>
+                                <tr class='divide-x divide-gray-400  border-gray-400 border'>
+                                    <th class="px-2 text-left">Nama Barang</th>
+                                    <th class="px-2 text-left">Ukuran</th >
+                                    <th class="px-2 text-left">QTY</th >
+                                    <th class="px-2 text-left">Ceklis</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($order->seragams as $seragam)
+                                
+                                    <tr data-seragam='{{ $seragam->id }}' class='label-seragam divide-x divide-gray-400  border-gray-400 border {{ $seragam->tersedia? 'bg-green-300':'bg-red-300' }}'>                                        
+                                            <td class="py-3 px-2 text-left">{{ $seragam->nama_barang }}</td>
+                                            <td class="py-3 px-2 text-left">{{ $seragam->ukuran }}</td>
+                                            <td class="py-3 px-2 text-left">{{ $seragam->kuantitas }}</td>
+                                            <td class="py-3 tersedia-status px-2 text-left">{{ $seragam->tersedia? 'Sudah' : 'Belum' }}</td>
+                                    </tr>
+                                <input class="seragam-input hidden" type="checkbox" name="seragam_ids[]" id="{{ $seragam->id }}" {{ $seragam->tersedia? 'checked' : '' }} value="{{ $seragam->id  }}"   />
+                                @endforeach
+                            </tbody>
+                       </table>
                 </div>
 
-                <div class="flex flex-col gap-5">
+                <div class="flex flex-col gap-10">
                     <button type="submit" name="action" value="complete"
                         class="w-[181px] h-[59px] bg-[#6F19DC] text-xl font-bold text-white rounded-xl border-white border"
-                        style="box-shadow: 2px 4px 6px 0 #7b39d2">Siap</button>
+                        style="box-shadow: 2px 4px 6px 0 gray">Siap</button>
                     <button type="submit" name="action" value="draft"
                         class="w-[181px] h-[59px] bg-[#2BCB4E] text-xl font-bold text-white rounded-xl border-white border"
-                        style="box-shadow: 2px 4px 6px 0 #4ef171">Simpan</button>
+                        style="box-shadow: 2px 4px 6px 0 gray">Simpan</button>
                 </div>
             </form>
         </div>
     </div>
+
+<script>
+    document.querySelectorAll(".label-seragam").forEach(function(label){
+        label.addEventListener("click", function(){
+            console.log("clicked")
+            const seragamId = label.dataset.seragam
+            console.log(seragamId)
+
+            document.querySelectorAll(".seragam-input").forEach(function(input){
+                if(seragamId === input.id){
+                    if(input.checked){
+                        input.checked = false;
+                        console.log(label)
+                        label.classList.remove("bg-green-300");
+                        label.classList.add("bg-red-300");
+                        label.querySelector(".tersedia-status").textContent = "Belum"
+                    } else {
+                        console.log(label)
+                        input.checked = true;
+                        label.classList.remove("bg-red-300");
+                        label.classList.add("bg-green-300");
+                        label.querySelector(".tersedia-status").textContent = "Sudah"           
+                    }
+                } else {
+                    if (!document.querySelector(`.seragam-input[id="${seragamId}"]:checked`)) {
+                    label.classList.remove("bg-green-300");
+                    label.classList.add("bg-red-300");
+                    label.querySelector(".tersedia-status").textContent = "Belum"
+                    }
+                }
+
+                
+            })
+        })
+    })
+
+
+</script>
 @endsection
+
+
