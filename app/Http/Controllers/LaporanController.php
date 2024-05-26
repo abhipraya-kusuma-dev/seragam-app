4e8571889d0e437fea5b2d50f4356941727a482b
WllamSB <sembiringwilliam7@gmail.com>
2024-05-26 15:37:49 +0700

    feat: filtering date & jenjang

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -14,11 +14,25 @@ use Illuminate\Support\Facades\DB;
 class LaporanController extends Controller
 {
     public function lihatOrderan(Request $request){
+
+        $inputUser = $request->tanggal;
+        $jenjang = $request->jenjang;
+        
+
         $orders = DB::table('seragams')
         ->join('statuses', 'seragams.id','=','statuses.seragam_id')
-        ->select('seragams.nama_barang','seragams.ukuran',DB::raw('SUM(statuses.kuantitas) as QTY'),'seragams.jenjang','seragams.id','seragams.harga')
-        ->groupBy('seragams.id', 'seragams.nama_barang', 'seragams.ukuran', 'seragams.jenjang', 'seragams.harga')
+        ->join('orders', 'orders.id', '=', 'statuses.order_id')
+        ->when($inputUser, function(Builder $builder) use($inputUser){
+            return $builder->whereDate('orders.created_at','=',Carbon::parse($inputUser));
+        })
+        ->when($jenjang, function(Builder $builder) use($jenjang){
+            return $builder->where('seragams.jenjang','LIKE', "%$jenjang%");
+        })
+        ->select('seragams.nama_barang','seragams.ukuran',DB::raw('SUM(statuses.kuantitas) as QTY'),'seragams.jenjang','seragams.id','seragams.harga', 'orders.created_at')
+        ->groupBy('seragams.id', 'seragams.nama_barang', 'seragams.ukuran', 'seragams.jenjang', 'seragams.harga','orders.created_at')
         ->get();
+
+        
 
         foreach($orders as $d){
             $status = DB::table('statuses')
@@ -32,7 +46,10 @@ class LaporanController extends Controller
         }
         
     }
+
         dd($orders->unique('nama_barang'));
+
+        
     }
-    
+  
 }
