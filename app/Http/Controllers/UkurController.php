2beb415a74c96b5fca043bd8c6d770f8f7069293
AdiCahyaSaputra <adi.jsonparse@gmail.com>
2024-06-08 15:51:56 +0700

    fix: gagal bikin order undefined seragam_id message

--- app/Http/Controllers/UkurController.php
+++ app/Http/Controllers/UkurController.php
@@ -213,8 +213,7 @@ class UkurController extends Controller
     } catch (Exception $e) {
       DB::rollBack();
 
-      return "Gagal bikin order:" . $e->getMessage();
-      // return back()->with('create-error', 'Gagal bikin order ' . $e->getMessage());
+      return back()->with('create-error', 'Gagal bikin order ' . $e->getMessage());
     }
   }
 
